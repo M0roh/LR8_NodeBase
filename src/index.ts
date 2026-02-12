068b491e2b6ae1b07bfab3bf86dbc7a6d42d04b8
Морон . <sisandr333@gmail.com>
2026-02-12 19:55:28 +0300

    refactor: migrated from js to ts

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+import * as osTask from './os/index.ts';
+import { config } from 'dotenv';
+import { FileDB } from './fileManager.ts';
+
+config();
+
+console.log("Имя: " + process.env.NAME);
+console.log("Фамилия: " + process.env.SURNAME);
+console.log("Группа: " + process.env.GROUP);
+console.log("Номер по журналу: " + process.env.JOURNAL_NUMBER);
+
+
+osTask.checkMemory();
+osTask.runWithAccess();
