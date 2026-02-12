3724c2ca6fcfac7ba9d39cfd5b07b010f98a9df5
Морон . <sisandr333@gmail.com>
2026-02-12 16:04:02 +0300

    feat: Added dotenv task

---
+++ src/index.js
@@ -0,0 +1,7 @@
+// 1
+require('dotenv').config();
+
+console.log("Имя: " + process.env.NAME)
+console.log("Фамилия: " + process.env.SURNAME)
+console.log("Группа: " + process.env.GROUP)
+console.log("Номер по журналу: " + process.env.JOURNAL_NUMBER)
